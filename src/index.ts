71af1cb15b6a7a3ff84108b36dfee20c4c668a02
JessicaPrior <jsscprior@gmail.com>
2020-12-07 15:44:21 -0300

    deploy2

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use("/user", userRouter)
 app.use("/image", imageRouter)
 
-const server = app.listen(3003, () => {
+const server = app.listen(3306 || 5000, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
